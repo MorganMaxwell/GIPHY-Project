5bcda8da9cfeda8d69be8e9fb673af1a6fb0f8f5
Morgan M <morgan.maxwell20@gmail.com>
2018-12-03 17:26:35 -0700

    API is working, now I need to be able to toggle the images

--- logic.js
+++ logic.js
@@ -1,24 +1,31 @@
 //wait for page to load completely before doing anything
 $(document).ready(function () {
-    //onclick event to start button creation
-    $(".CreateButton").on("click", createButton());
-
     // create a new button with value and label === to what the user input
-    // function createButton() {
-    //     var userInput = $(".searchBar").val();
-    //     var makeButton = $('button');
-    //     makeButton.text(userInput);
-    //     console.log(userInput);
-    //     console.log(makeButton);
-    //     // $('buttonBox').append(makeButton);
-    // };
+    function createButton() {
+        var userInput = $(".searchBar").val();
+        var newButton = $('<button>').append(userInput);
+        newButton.attr('id', userInput);
+        $('.buttonBox').append(newButton);
+    };
 
 
-    // grab data
-    // var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + + "&api_key=&limit=5";
 
-    // $.ajax({ url: queryURL, method: "GET" })
-    //     .then(function () {
+    function GIFGetter() {
+        // grab data
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + $(this) + "&api_key=8YFcTyyCHtCNjQVi55eFHpokpZSNe5eX&limit=10";
 
-    //     });
+        $.ajax({ url: queryURL, method: "GET" })
+            .then(function (response) {
+                $('.responsesBox').empty();
+                for (var i = 0; i < (response.data).length; i++) {
+                    var result = response.data[i].images;
+                    var createGif = $('<img>');
+                    createGif.attr("src", result.fixed_height.url);
+                    $('.responsesBox').append(createGif);
+                };
+            });
+    };
+    // onclick to create a button
+    $(".CreateButton").on("click", createButton);
+    $(".buttonBox").on("click", GIFGetter);
 });
